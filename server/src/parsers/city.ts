0e2458e5143ae2abbdf7e79dd76813fac04c8d05
arthot <ktf.labs@gmail.com>
2017-10-16 03:04:06 +0300

    Cities parser

---
+++ server/src/parsers/city.ts
@@ -0,0 +1,40 @@
+import { City } from 'src/common/city'
+import { request } from 'src/utils/request'
+
+const CityRegex = new RegExp('[C,M,T]');
+
+export function parse(name: string, lang: string) {
+    if (!name) throw new Error('city name should not be empty')
+    if (name.length < 2) throw new Error('city name lenght must be not less than 2')
+
+    const url = `https://www.gismeteo.${(lang == "ru" ? "ru" : "com")}/ajax/suggest/?lang=${lang}&startsWith=${name}&sort=typ`
+
+    return request(url)
+        .then(r => (<CityApi[]>r.items)
+            .filter(c => c.kind && CityRegex.test(c.kind))
+            .map(c => new City(
+                c.id,
+                c.lang,
+                { code: c.country_code, name: c.country_name },
+                c.kind,
+                c.district_name,
+                c.name,
+                c.sub_district_name
+            )))
+}
+
+interface CityApi {
+    id: number;
+    district_id: number;
+    sub_district_id: string;
+    country_id: number;
+    country_code: string;
+    kind: string;
+    country_name: string;
+    district_name: string;
+    sub_district_name: string;
+    site_name: string;
+    name: string;
+    lang: string;
+    type: string;
+}
---
+++ server/src/utils/request.ts
@@ -0,0 +1,12 @@
+import * as req from 'request-promise-native'
+
+export function request(url: string) {
+    return req(url, {
+        jar: true,
+        gzip: true,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.38 Safari/537.36 OPR/49.0.2725.18'
+        },
+        json: true
+    })
+}
