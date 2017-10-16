5a663f1939a210d0f3592119b2768914d51f2a70
Artsiom Khatsianovich <a.khatsianovich@godeltech.com>
2017-10-16 19:00:01 +0300

    temp

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,11 @@
-import "reflect-metadata";
-import * as Koa from "koa";
-import * as bodyParser from "koa-bodyparser";
+import 'reflect-metadata'
+import * as Koa from 'koa'
+import * as bodyParser from 'koa-bodyparser'
+import * as Knex from 'knex'
+
+const knexConfig = require('../knexfile');
+
+const knex = Knex(knexConfig.development);
 
 const app = new Koa();
 
@@ -8,4 +13,4 @@ app.use(bodyParser());
 
 app.listen(3003);
 
-console.log("Application is up and running on port 3003");
+console.log('Application is up and running on port 3003');
