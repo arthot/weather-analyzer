b27fb18862c30d27a85491e4de017ad5a90ed404
arthot <ktf.labs@gmail.com>
2018-01-14 15:10:55 +0300

    Make browser history controlled

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,24 +1,27 @@
+import createHistory from 'history/createBrowserHistory'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { Route } from 'react-router'
+import { ConnectedRouter } from 'react-router-redux'
 import { Root } from 'src/components/global/Root'
 import { configureStore } from 'src/store'
 
 require('src/styles/common.less')
 
-const store = configureStore();
+const history = createHistory();
+const store = configureStore(history);
 
 const render = (App: React.ComponentClass, hydrate = false) => {
   const container = document.getElementById('root');
   const app = (
     <AppContainer>
-      <BrowserRouter>
-        <Provider store={store}>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
           <App />
-        </Provider>
-      </BrowserRouter>
+        </ConnectedRouter>
+      </Provider>
     </AppContainer>
   );
   if (hydrate) {
