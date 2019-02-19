6f6606e0511a17f333f234f379a206f60c934e4b
arthot <ktf.labs@gmail.com>
2019-02-19 18:17:18 +0300

    Remove index page rendering functionality

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,27 +1,14 @@
-import 'reflect-metadata'
 import * as express from 'express'
 import './store/objection'
 import { routes as apiRoutes } from 'src/routes'
-import { routes as staticRoutes } from 'src/static'
 import { config } from 'src/utils/config'
 import { getLogger } from 'src/utils/log'
 
-const mustacheExpress = require('mustache-express');
 const log = getLogger(module);
 const app = express();
 
 app.use('/api', apiRoutes);
-app.use(staticRoutes);
-
-app.engine('mustache', mustacheExpress());
-
-app.set('view engine', 'mustache');
-app.set('views', __dirname + '/views');
 
 export const server = app.listen(config.port, () => {
     log.info(`Application is up and running on port ${config.port}`);
-
-    if (process.send) {
-        process.send('online');
-    }
 });
--- server/src/utils/locale.ts
+++
@@ -1,7 +0,0 @@
-import { Request } from 'express'
-
-const Langs = ['en', 'ru'];
-
-export function detectLocale(req: Request) {
-    return req.acceptsLanguages().find(l => Langs.includes(l)) || 'en';
-}
