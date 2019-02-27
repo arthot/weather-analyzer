29e6cce1045feabd6941a0ef9fab84b19e561ed5
arthot <ktf.labs@gmail.com>
2019-02-27 22:18:21 +0300

    Fix localization

--- client/src/store.js
+++ client/src/store.js
@@ -2,6 +2,7 @@ import { applyMiddleware, compose, createStore } from 'redux'
 import saga from 'redux-saga'
 import { store as rootReducer } from './reducers'
 import rootSaga from './saga'
+import i18 from './locale/i18n'
 
 export default (context = {}) => {
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
@@ -13,6 +14,8 @@ export default (context = {}) => {
                 sagaMiddleware,
             ),
         ));
+
+    i18(store.getState().locale);
 
     sagaMiddleware.run(rootSaga, context);
 
