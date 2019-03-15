6a5558623ee4c9ed354d576d60f4167e82a2e8ac
arthot <ktf.labs@gmail.com>
2019-03-15 21:52:35 +0300

    Temperature-based background color generation

---
+++ client/src/components/weather/main/Day/colors.js
@@ -0,0 +1,40 @@
+export const MAX = 40;
+export const MIN = -30;
+
+/*
+    Linear gradient with the folling structure from top to bottom: background | colored border | color
+*/
+const backGradient = (percent, r, g, b) => `linear-gradient(to bottom, 
+    white 0%,
+    white ${100 - percent}%,
+    rgba(${r}, ${g}, ${b}, 1) ${100 - percent}%,
+    rgba(${r}, ${g}, ${b}, 1) ${100 - percent + 3}%,
+    rgba(${r}, ${g}, ${b}, 0.7) ${100 - percent + 3}%,
+    rgba(${r}, ${g}, ${b}, 0.7) 100%)`;
+
+/*
+    This code is a pentagone path on hue diagram from red to blue colors replacing green part with minty colors
+*/
+export function getTemperatureColor(percent) {
+    if (percent >= 80) {
+        return backGradient(percent,
+            255,
+            (103 - percent) * 10,
+            0);
+    } else if (percent >= 60) {
+        return backGradient(percent,
+            255,
+            (264 - percent) * 1.25,
+            (100 - percent) * 2.5);
+    } else if (percent >= 40) {
+        return backGradient(percent,
+            (27.1 - percent) * -7.75,
+            (-144 - percent) * -1.25,
+            (72.9 - percent) * 7.75);
+    } else {
+        return backGradient(percent,
+            (0 - percent) * -2.5,
+            (0 - percent) * -5.75,
+            255);
+    }
+}
--- client/src/components/weather/main/Day/index.jsx
+++ client/src/components/weather/main/Day/index.jsx
@@ -1,13 +1,25 @@
 import React, { PureComponent } from 'react'
+import { MIN, MAX, getTemperatureColor } from './colors'
 
 export default class Day extends PureComponent {
     render() {
         const { day, data } = this.props;
-        console.log(data);
-        
-        return (
-            <div className="data-day">{day}</div>
-        )
+
+        if (data) {
+            const percent = (Math.max(Math.min(MAX, data.temperature), MIN) - MIN) / (MAX - MIN) * 100;
+            const gradient = getTemperatureColor(percent);
+
+            return (
+                <div className="data-day" style={{ backgroundImage: gradient }}>
+                    <span className="data-day-temperature">{data.temperature}</span>
+                </div>
+            )
+        } else {
+            return (
+                <div className="data-day">
+                </div>
+            )
+        }
     }
 }
 
