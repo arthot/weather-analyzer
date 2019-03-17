9efe34f58c01763826a4cd754743ae171f9583a0
arthot <ktf.labs@gmail.com>
2019-03-17 20:49:30 +0300

    Enhance parser request headers

--- server/src/utils/request.ts
+++ server/src/utils/request.ts
@@ -1,12 +1,16 @@
 import * as req from 'request-promise-native'
 
+const headers = {
+    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8,ru;q=0.7',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.38 Safari/537.36 OPR/49.0.2725.18',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+}
+
 export function request(url: string) {
     return req(encodeURI(url), {
         jar: true,
         gzip: true,
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.38 Safari/537.36 OPR/49.0.2725.18'
-        },
+        headers,
         json: true
     })
 }
@@ -15,8 +19,6 @@ export function requestPage(url: string) {
     return req(encodeURI(url), {
         jar: true,
         gzip: true,
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.38 Safari/537.36 OPR/49.0.2725.18'
-        }
+        headers,
     })
 }
