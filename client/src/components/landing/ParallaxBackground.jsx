dda39351350fbe50c297d21ebff643a8b6f31c2c
arthot <ktf.labs@gmail.com>
2019-05-01 12:08:37 +0300

    Move paralax images out of css to optimize render

--- client/src/components/landing/ParallaxBackground.jsx
+++ client/src/components/landing/ParallaxBackground.jsx
@@ -1,13 +1,18 @@
 import React, { PureComponent } from 'react'
 import ParallaxJs from 'parallax-js'
 
+import LayerImg1 from '../../images/landscape1.svg';
+import LayerImg2 from '../../images/landscape2.svg';
+import LayerImg3 from '../../images/landscape3.svg';
+import LayerImg4 from '../../images/landscape4.svg';
+
 export default class ParallaxBackground extends PureComponent {
 
     paralaxInstance = null
     wrapEl = React.createRef()
 
     componentDidMount() {
-        this.paralaxInstance = new ParallaxJs(this.wrapEl.current, this.props.options);
+        this.paralaxInstance = new ParallaxJs(this.wrapEl.current, {});
 
         if (/firefox/i.test(navigator.userAgent))
             this.paralaxInstance.disable();
@@ -21,10 +26,10 @@ export default class ParallaxBackground extends PureComponent {
     render() {
         return (
             <div ref={this.wrapEl} className="paralax-background">
-                <div data-depth="0.05" className="paralax-layer paralax-layer-4"></div>
-                <div data-depth="0.15" className="paralax-layer paralax-layer-3"></div>
-                <div data-depth="0.3" className="paralax-layer paralax-layer-2"></div>
-                <div data-depth="0.5" className="paralax-layer paralax-layer-1"></div>
+                <div data-depth="0.05" className="paralax-layer"><img src={LayerImg4} /></div>
+                <div data-depth="0.15" className="paralax-layer"><img src={LayerImg3} /></div>
+                <div data-depth="0.3" className="paralax-layer"><img src={LayerImg2} /></div>
+                <div data-depth="0.5" className="paralax-layer"><img src={LayerImg1} /></div>
             </div>
         )
     }
