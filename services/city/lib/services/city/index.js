d32b0d8eb51032e6f6d262dcce531ccda817f6e9
arthot <ktf.labs@gmail.com>
2020-08-30 21:57:31 +0300

    City parser tests

--- services/city/lib/services/city/index.js
+++ services/city/lib/services/city/index.js
@@ -9,6 +9,7 @@ const headers = {
   'User-Agent':
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 OPR/69.0.3686.77',
   Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+  'content-type': 'application/json',
 };
 
 const cookieStore = new Map();
