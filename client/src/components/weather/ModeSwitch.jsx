5e2f040c89ac249087c2cd91514ac73253c54015
arthot <ktf.labs@gmail.com>
2021-01-06 15:30:40 +0300

    Update deps

--- client/src/components/weather/ModeSwitch.jsx
+++ client/src/components/weather/ModeSwitch.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import i18n from 'es2015-i18n-tag'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import classNames from 'classnames'
 
 import * as ACTIONS from '../../weather/actions'
 import { MODES } from '../../weather/constants'
